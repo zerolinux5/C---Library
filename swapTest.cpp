e0111e021cc4b6530c33ef6f9e4a6a39de428110
Jesus Magana <maganazero5@gmail.com>
2014-07-23 11:38:18 -0700

    Also show string swap

--- swapTest.cpp
+++ swapTest.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "swap.h"
 
 using namespace std;
@@ -6,9 +7,16 @@ using namespace std;
 int main()
 {
 	int a = 5;
-	int b = 7;
+	int b = 7i;
+
+	string s1("abcdef"), s2("1234");	
+
 	cout << "a, b = " << a << " " << b << endl;
 	swap(a,b);
 	cout << "a, b = " << a << " " << b << endl;
+
+	cout << "s1, s2 = " << s1 << " " << s2 << endl;
+	swap(s1,s2);
+	cout << "s1, s2 = " << s1 << " " << s2 << endl;
 	return 0;
 }
