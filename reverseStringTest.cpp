323e29d54d1b5b82a7ac97fd27cf36dfa10cc34b
Jesus Magana <maganazero5@gmail.com>
2014-07-23 12:12:44 -0700

    Added function and basic return

--- reverseStringTest.cpp
+++ reverseStringTest.cpp
@@ -2,10 +2,16 @@
 #include <string>
 using namespace std;
 
+string reverseString(string inputString)
+{
+	return inputString;
+}
+
 int main()
 {	
 	string s1("This is a string");
 	cout << "s1 contains: " << s1 << endl;
-
+	string s2 = reverseString(s1);
+	cout << "s2 contains: " << s2 << endl;
 	return 0;
 } 
