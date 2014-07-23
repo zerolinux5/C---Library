526c7bf97f75bb6925b0f908315004721dfc6491
Jesus Magana <maganazero5@gmail.com>
2014-07-23 12:21:16 -0700

    Reverse string via recursion

--- reverseStringTest.cpp
+++ reverseStringTest.cpp
@@ -12,11 +12,22 @@ string reverseString(string inputString)
 	return returnString;
 }
 
+string reverseStringR(string inputString)
+{
+	if (inputString.length() < 2){
+		return inputString;
+	} else {
+		return  reverseStringR(inputString.substr(1,inputString.length())) + inputString[0];
+	}
+}
+
 int main()
 {	
 	string s1("This is a string");
 	cout << "s1 contains: " << s1 << endl;
 	string s2 = reverseString(s1);
 	cout << "s2 contains: " << s2 << endl;
+	string s3 = reverseStringR(s1);
+	cout << "s3 contains: " << s3 << endl;
 	return 0;
 } 
