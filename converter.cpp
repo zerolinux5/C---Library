ca485eb9445ae168bbf6d2f124550432a780ac1f
Jesus Magana <maganazero5@gmail.com>
2014-07-24 21:26:22 -0700

    Added logic

--- converter.cpp
+++ converter.cpp
@@ -2,10 +2,39 @@
 
 using namespace std;
 
+double tempConverter(double temp, string from, string to)
+{
+	if(from.compare("K") == 0){
+		double kToC = temp - 273.15;
+		if(to.compare("C") == 0){
+			return kToC;
+		} else {
+			return (kToC * 1.8 + 32);
+		}
+	} else if (from.compare("C") == 0){
+		if(to.compare("K") == 0){
+			return (temp+273.15);
+		} else {
+			return (temp * (9/5) + 32);
+		}
+	} else {
+		if (to.compare("C") == 0){
+			return ((temp - 32) * (5/9));
+		} else {
+			return ((5/9)*(temp-32) + 273.15);
+		}
+	}
+}
+
 int main()
 {
-
-        cout << "Hello converter" << endl;
-        return 0;
+	double temperature = 100;
+	double k = tempConverter(temperature, "C", "K");
+	double C = tempConverter(temperature, "K", "C");
+	double F = tempConverter(temperature, "K", "F");
+	cout << "K:" << k << endl; 
+	cout << "C:" << C << endl; 
+	cout << "F:" << F << endl; 
+	return 0;
 }
 
