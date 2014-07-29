f5ae0a5913c424ce61b578853749d1fb3e3f86b1
Jesus Magana <maganazero5@gmail.com>
2014-07-28 21:38:54 -0700

    logic via itteration works

--- factorial.cpp
+++ factorial.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+double factorialIt(int given)
+{
+	int factorialNum = given;
+	for (int i = 1; i < given ;i++){
+		factorialNum *= i;
+	}
+	return factorialNum;
+}
+
 int main()
 {
-	cout << "Hello" << endl;
+	int factIn = 10;
+	double factOut = factorialIt(factIn);
+	cout << "factin:" << factIn << " factOut:" << factOut << endl;
 	return 1;
 }
